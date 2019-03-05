959181666c4996793800f59912063de925c0435e
nimomaina <nimomaina.nm@gmail.com>
2019-03-05 10:22:44 +0300

    Create testcases for models

--- images/tests/tests.py
+++ images/tests/tests.py
@@ -8,7 +8,7 @@ class ImageTestCase(TestCase):
     def setUp(self):
         self.loc =Location(name='Nairobi')
         self.loc.save_loc()
-        self.new= Picture(name='My Picture', description='Photo taken by me', category='food', location='nairobi',image='default.jpg')
+        self.new= Picture(name='My Picture', description='Photo taken by me', category='food', location=self.loc,image='default.jpg')
         self.new.save_pic()
 
 
@@ -27,31 +27,24 @@ class ImageTestCase(TestCase):
         self.assertTrue(len(pics) < 0)
 
 
+class LocationTestCase(TestCase):
 
-class Location(TestCase):
+    # set up method
     def setUp(self):
-        self.nimo = Editor(first_name='Nimo', last_name='Maina', email='nimo@example.com', phone_number='0712345678')
-        self.nimo.save_editor()
+        self.loc =Location(name='Nairobi')
+        self.loc.save_loc()
 
-        # Create a new tag and save it
-        self.new_tag= tags(name='testing')
-        self.new_tag.save()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.loc,Location))
 
-        self.new_article=Article(title = 'Test Article',post='This is a random test Post',editor = self.nimo)
-        self.new_article.save()
-        self.new_article.tags.add(self.new_tag)
+    def test_save_method(self):
+        self.loc.save_loc()
+        locs=Location.objects.all()
+        self.assertTrue(len(locs) > 0)
 
-    def tearDown(self):
-        Editor.objects.all().delete()
-        tags.objects.all().delete()
-        Article.objects.all().delete()
 
-    def test_get_news_today(self):
-        today_news=Article.todays_news()
-        self.assertTrue(len(today_news)>0)
+    def test_delete_method(self):
+        self.loc.delete_loc()
+        locs=Location.objects.all()
+        self.assertTrue(len(locs) < 0)
 
-    def test_get_news_by_dae(self):
-        test_date = '2019-02-25'
-        date = dt.datetime.strptime(test_date,'%Y-%m-%d').date()
-        news_by_date=Article.days_news(date)
-        self.assertTrue(len(news_by_date) == 0)
